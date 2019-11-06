2f5cb8181c6847fccddbaf67f5dbcd88d87d07f4
GreenNun <stas909@gmail.com>
2019-11-06 22:06:31 +0300

    avsc with bigdecimal

--- kafka-avro/src/main/java/eu/bausov/kafkaavro/KafkaAvroApplication.java
+++ kafka-avro/src/main/java/eu/bausov/kafkaavro/KafkaAvroApplication.java
@@ -1,6 +1,5 @@
 package eu.bausov.kafkaavro;
 
-import eu.bausov.kafkaavro.model.Weather;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,12 +8,5 @@ public class KafkaAvroApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KafkaAvroApplication.class, args);
-        test();
-    }
-
-    private static void test() {
-        final var weather = new Weather();
-        final var build = Weather.newBuilder().build();
-        System.out.println(weather);
     }
 }
---
+++ kafka-avro/src/main/java/eu/bausov/kafkaavro/TestAvsc.java
@@ -0,0 +1,24 @@
+package eu.bausov.kafkaavro;
+
+import eu.bausov.kafkaavro.model.Weather;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by GreenNun on 06.11.2019.
+ */
+public class TestAvsc {
+
+    public static void main(String[] args) {
+        final var weather = new Weather();
+        System.out.println(weather);
+
+        final var build = Weather.newBuilder()
+                .setTime(1L)
+                .setStation("station")
+                .setTemp(20)
+                .setPrice(new BigDecimal(22))
+                .build();
+        System.out.println(build);
+    }
+}
